2810786f5d8c100e130be60d8c6bcba5e6a00684
NastyaGavrichenkova <lady_markos@mail.ru>
2023-11-19 14:10:36 +0900

    fix web conftest after merge

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,49 +1,19 @@
 import pytest
 from selene import browser
-from selenium import webdriver
-import os
-from dotenv import load_dotenv
 
 from utils import attach
-from utils.helper import abs_path_to_file
+from config import Config
 
-load_dotenv(abs_path_to_file('.env.selenoid_credentials'))
-
-DEFAULT_BROWSER_VERSION = '100.0'
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default=DEFAULT_BROWSER_VERSION
-    )
+config = Config()
 
 
 @pytest.fixture(autouse=True)
 def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
-    driver_options = webdriver.ChromeOptions()
+    browser.config.driver = config.to_browser_driver_options
 
-    browser.config.driver_options = driver_options
+    if config.web_context == 'remote':
+        browser.config.driver = config.browser_remote_driver()
 
-    selenoid_capabilities = {
-        'browserName': 'chrome',
-        'browserVersion': browser_version,
-        'selenoid:options': {
-            'enableVNC': True,
-            'enableVideo': True
-        }
-    }
-    driver_options.capabilities.update(selenoid_capabilities)
-
-    login = os.getenv('SELENOID_LOGIN')
-    password = os.getenv('SELENOID_PASSWORD')
-
-    browser.config.driver = webdriver.Remote(
-        command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
-        options=driver_options
-    )
     browser.config.base_url = 'https://open.spotify.com'
 
     yield browser
