e4aba123b08c1a91f86cf1012c3fec26fbe9ca6c
NastyaGavrichenkova <lady_markos@mail.ru>
2023-11-19 12:32:26 +0900

    remove add --browser_version running option

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -9,27 +9,16 @@ from utils.helper import abs_path_to_file
 
 load_dotenv(abs_path_to_file('.env.selenoid_credentials'))
 
-DEFAULT_BROWSER_VERSION = '100.0'
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default=DEFAULT_BROWSER_VERSION
-    )
-
 
 @pytest.fixture(autouse=True)
 def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
     driver_options = webdriver.ChromeOptions()
 
     browser.config.driver_options = driver_options
 
     selenoid_capabilities = {
         'browserName': 'chrome',
-        'browserVersion': browser_version,
+        'browserVersion': '100.0',
         'selenoid:options': {
             'enableVNC': True,
             'enableVideo': True
